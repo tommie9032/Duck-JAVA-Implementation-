302b2a05eb312108368d6954fcb4d47892c19ba1
tommie9032 <tomthomas9032@gmail.com>
2025-03-08 19:15:20 +0530

    Final DecoyDuck

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -1,13 +1,14 @@
+// DecoyDuck class extending the Duck base class
 public class DecoyDuck extends Duck {
 
-
+    // Constructor to set specific behaviors for DecoyDuck
     public DecoyDuck() {
-        quackBehaviour = new Mute();
-        swimBehaviour = new Drowning();
-        flightBehaviour = new Fall();
-
-
+        quackBehaviour = new Mute();      // Assigning mute behavior (no quacking capability)
+        swimBehaviour = new Drowning();   // Assigning drowning behavior (not an active swimmer)
+        flightBehaviour = new Fall();     // Assigning falling behavior (no flight capability)
     }
+
+    // Method to display the type of duck
     @Override
     public void display() {
         System.out.println("I am Decoy Duck !!");
