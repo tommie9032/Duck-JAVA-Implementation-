67ea7a6369289509e02a40dca6735a4456a3d40e
tommie9032 <tomthomas9032@gmail.com>
2025-03-08 19:16:38 +0530

    Final MallardDuck

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,9 +1,14 @@
-public class MallardDuck extends Duck{
-    public MallardDuck(){
-        quackBehaviour = new Quack();
-        swimBehaviour = new SwimWithLegs();
-        flightBehaviour = new Fly();
+// MallardDuck class extending the Duck base class
+public class MallardDuck extends Duck {
+
+    // Constructor to set specific behaviors for MallardDuck
+    public MallardDuck() {
+        quackBehaviour = new Quack();       // Assigning quack behavior
+        swimBehaviour = new SwimWithLegs(); // Assigning swimming behavior
+        flightBehaviour = new Fly();        // Assigning flight behavior
     }
+
+    // Method to display the type of duck
     @Override
     public void display() {
         System.out.println("I am Mallard Duck !!");
