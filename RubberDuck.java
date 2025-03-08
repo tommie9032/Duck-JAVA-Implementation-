b9035c11ecb8fd198aebb2772d1ffbeb31541c90
tommie9032 <tomthomas9032@gmail.com>
2025-03-08 19:14:18 +0530

    Final RubberDuck

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,13 +1,14 @@
+// RubberDuck class extending the Duck base class
 public class RubberDuck extends Duck {
 
-
+    // Constructor to set specific behaviors for RubberDuck
     public RubberDuck() {
-        quackBehaviour = new Squeak();
-        swimBehaviour = new Floating();
-        flightBehaviour = new Fall();
-
-
+        quackBehaviour = new Squeak();   // Assigning squeaking behavior
+        swimBehaviour = new Floating();  // Assigning floating behavior
+        flightBehaviour = new Fall();    // Assigning falling behavior (no flight capability)
     }
+
+    // Method to display the type of duck
     @Override
     public void display() {
         System.out.println("I am Rubber Duck !!");
