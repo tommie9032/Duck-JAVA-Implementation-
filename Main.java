23b5901125336e83c1637c2d2a58148506a49d2d
tommie9032 <tomthomas9032@gmail.com>
2025-03-08 19:11:01 +0530

    Final Main file

--- Main.java
+++ Main.java
@@ -1,32 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Main class to demonstrate different types of ducks
+//Name : Tom Thomas
+//PRN : 23070126136
+//Batch : 23070126136
 public class Main {
     public static void main(String[] args) {
+        // Creating a MallardDuck object and calling its behaviors
         MallardDuck md = new MallardDuck();
         System.out.println("---------------------");
-        md.display();
-        md.performQuack();
-        md.performSwim();
-        md.performFlight();
+        md.display();      // Displaying the type of duck
+        md.performQuack(); // Performing quack behavior
+        md.performSwim();  // Performing swim behavior
+        md.performFlight();// Performing flight behavior
         System.out.println("---------------------");
 
+        // Creating a RubberDuck object and calling its behaviors
         RubberDuck rd = new RubberDuck();
         System.out.println("---------------------");
-        rd.display();
-        rd.performQuack();
-        rd.performSwim();
-        rd.performFlight();
+        rd.display();      // Displaying the type of duck
+        rd.performQuack(); // Performing quack behavior
+        rd.performSwim();  // Performing swim behavior
+        rd.performFlight();// Performing flight behavior
         System.out.println("---------------------");
 
-
+        // Creating a RedHeadDuck object and calling its behaviors
         RedHeadDuck rh = new RedHeadDuck();
         System.out.println("---------------------");
-        rh.display();
-        rh.performQuack();
-        rh.performSwim();
-        rh.performFlight();
+        rh.display();      // Displaying the type of duck
+        rh.performQuack(); // Performing quack behavior
+        rh.performSwim();  // Performing swim behavior
+        rh.performFlight();// Performing flight behavior
         System.out.println("---------------------");
 
-
+        // Creating a DecoyDuck object and calling its behaviors
+        DecoyDuck dd = new DecoyDuck();
+        System.out.println("---------------------");
+        dd.display();      // Displaying the type of duck
+        dd.performQuack(); // Performing quack behavior
+        dd.performSwim();  // Performing swim behavior
+        dd.performFlight();// Performing flight behavior
+        System.out.println("---------------------");
     }
 }
--- Mute.java
+++ Mute.java
@@ -1,7 +1,7 @@
 public class Mute implements QuackBehaviour {
     @Override
     public void quack() {
-        System.out.println("Im a Mute Duck!!");
+        System.out.println("I am a Mute Duck!!");
 
     }
 }
